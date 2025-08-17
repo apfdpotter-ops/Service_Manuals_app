a8b8c361193a0d95522d3765f781c127db01c521
apfdpotter-ops <apfd.potter@gmail.com>
2025-08-16 23:00:10 -0500

    Update manuals.ts

--- pages/api/manuals.ts
+++ pages/api/manuals.ts
@@ -1,6 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import data from "../../data/manuals.json";
-
+const response = await fetch(
+  "https://raw.githubusercontent.com/apfdpotter-ops/Service_Manuals_app/main/data/manuals.json"
+);
+const manuals = await response.json();
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(data);
 }
